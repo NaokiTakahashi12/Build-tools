d374cb987d834b4c1c00dc922d82f89b1afc4e87
Anonymous <>
2018-10-13 23:51:04 +0900

    Fixed create module

--- cmake/create_module.sh
+++ cmake/create_module.sh
@@ -3,8 +3,8 @@
 mkdir -p NonNameModule/build && cp `dirname $0`/CMakeLists.txt NonNameModule/build/
 
 if [ "$(uname)" == 'Darwin' ]; then
-	gsed -i -e "12i set(CMAKE_MODULE_PATH \${CMAKE_MODULE_PATH} `dirname $0`/module)" NonNameModule/build/CMakeLists.txt
+	gsed -i -e "12i set(CMAKE_MODULE_PATH \${CMAKE_MODULE_PATH} `cd $(dirname $0) && pwd`/module)" CMakeLists.txt
 else
-	gsed -i -e "12i set(CMAKE_MODULE_PATH \${CMAKE_MODULE_PATH} `dirname $0`/module)" NonNameModule/build/CMakeLists.txt
+	gsed -i -e "12i set(CMAKE_MODULE_PATH \${CMAKE_MODULE_PATH} `cd $(dirname $0) && pwd`/module)" CMakeLists.txt
 fi
 
